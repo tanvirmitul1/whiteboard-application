3776dbbf6be999ff15738c0f264ca9b18e788de0
tanvirmitul1 <mitulseopage1@gmail.com>
2024-09-21 13:52:22 +0600

    api route add

--- client/src/Apis/apiSlice.js
+++ client/src/Apis/apiSlice.js
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-const apiBaseUrl = "http://localhost:5000/api";
+const apiBaseUrl =
+  "http://localhost:5000/api" || process.env.REACT_APP_API_BASE_URL;
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
