85bebec5389d86acb8df74f89699cb98b11ee4b8
tanvirmitul1 <mitulseopage1@gmail.com>
2024-09-23 12:20:13 +0600

    layout authentication fix

--- client/src/pages/Layout.js
+++ client/src/pages/Layout.js
@@ -1,17 +1,28 @@
-// src/components/Layout.js
-import React from "react";
-import { useLocation } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import Navbar from "../components/NavBar";
+import useAuth from "../customHooks/useAuth";
 
 const Layout = ({ children }) => {
+  const { user } = useAuth();
   const location = useLocation();
+  const navigate = useNavigate();
   const isAuthRoute =
     location.pathname === "/" || location.pathname === "/change-password";
 
+  useEffect(() => {
+    // Redirect to homepage if there is no user and not on auth routes
+    if (!user && !isAuthRoute) {
+      navigate("/");
+    }
+  }, [user, isAuthRoute, navigate]);
+
   return (
     <div className="App">
-      {!isAuthRoute && <Navbar />}
-      <div>{children}</div>
+      <>
+        {!isAuthRoute && <Navbar />}
+        <div>{children}</div>
+      </>
     </div>
   );
 };
