66ba07bdc70c3fe2b1261d374c6846b5a8d2ea6e
tanvirmitul1 <mitulseopage1@gmail.com>
2024-09-26 09:41:46 +0600

    action buttons implemented

--- client/src/components/viewPage/ActionButton.js
+++ client/src/components/viewPage/ActionButton.js
@@ -1,22 +1,144 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  IconButton,
+  Popover,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ReportIcon from "@mui/icons-material/Report";
 import { toast } from "react-toastify";
-const ActionButton = () => {
-  const handleAction = () => {
-    console.log("Action not implemented yet");
-    toast.error("Action not implemented yet");
+import "react-toastify/dist/ReactToastify.css";
+import useColors from "../../customHooks/useColors";
+
+// Dark mode colors
+const darkModeColors = {
+  background: "#1b1b1b",
+  text: "#fff",
+  iconColor: "#bbb",
+  hover: "#555",
+};
+
+const ActionButton = ({ whiteboard, handleEditClick, handleDeleteClick }) => {
+  const { colors } = useColors();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
   };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleAction = (action) => {
+    switch (action) {
+      case "edit":
+        handleEditClick(whiteboard);
+
+        console.log("edit");
+        break;
+      case "delete":
+        handleDeleteClick(whiteboard._id);
+        break;
+      case "report":
+        toast.error(`${action} action not implemented yet`);
+        break;
+      default:
+        break;
+    }
+
+    handleClose();
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
   return (
-    <div
-      onClick={handleAction}
-      style={{
-        cursor: "pointer",
-        color: "White",
-        position: "absolute",
-        top: "10px",
-        right: "10px",
-      }}
-    >
-      ...
+    <div>
+      <IconButton
+        aria-describedby={id}
+        onClick={handleClick}
+        style={{
+          color: colors.textColor,
+          position: "absolute",
+          top: "15px",
+          right: "10px",
+        }}
+      >
+        <MoreVertIcon />
+      </IconButton>
+
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+        PaperProps={{
+          style: {
+            backgroundColor: colors.popoverBg,
+            color: colors.textColor,
+            cursor: "pointer",
+          },
+        }}
+      >
+        <List>
+          <ListItem
+            button
+            onClick={() => handleAction("edit")}
+            style={{
+              color: darkModeColors.text,
+              "&:hover": { backgroundColor: colors.secondaryBgColor },
+            }}
+          >
+            <ListItemIcon style={{ color: colors.textColor }}>
+              <EditIcon />
+            </ListItemIcon>
+            <ListItemText primary="Edit" />
+          </ListItem>
+
+          <ListItem
+            button
+            onClick={() => handleAction("delete")}
+            style={{
+              color: darkModeColors.text,
+              "&:hover": { backgroundColor: colors.secondaryBgColor },
+            }}
+          >
+            <ListItemIcon style={{ color: colors.textColor }}>
+              <DeleteIcon />
+            </ListItemIcon>
+            <ListItemText primary="Delete" />
+          </ListItem>
+
+          <ListItem
+            button
+            onClick={() => handleAction("report")}
+            style={{
+              color: darkModeColors.text,
+              "&:hover": { backgroundColor: colors.secondaryBgColor },
+            }}
+          >
+            <ListItemIcon style={{ color: colors.textColor }}>
+              <ReportIcon />
+            </ListItemIcon>
+            <ListItemText primary="Report" />
+          </ListItem>
+        </List>
+      </Popover>
     </div>
   );
 };
